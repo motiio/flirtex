75257fb3c03068110166ae8a872f2a807843c65a
Matvey <motiio.none@gmail.com>
2023-05-18 00:19:30 +0300

    Fix

--- .space.kts
+++ .space.kts
@@ -21,32 +21,32 @@ job("Run on git push") {
     // }
     // }
 
-    host("Build and push a Docker image...") {
-        dockerBuildPush {
-            // by default, the step runs not only 'docker build' but also 'docker push'
-            // to disable pushing, add the following line:
-            // push = false
-
-            // path to Docker context (by default, context is working dir)
-//             context = "docker buildx -f docker/Dockerfile.test --platform linux/arm64 && echo"
-            // path to Dockerfile relative to the project root
-            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
-            file = "docker/Dockerfile.test"
-            // build-time variables
-            // image labels
-            labels["vendor"] = "connecta"
-//            platform = "linux/amd64,linux/arm64"
-            // to add a raw list of additional build arguments, use
-            extraArgsForBuildCommand = listOf("--no-cache")
-            // to add a raw list of additional push arguments, use
-            // extraArgsForPushCommand = listOf("...")
-            // image tags
-            tags {
-                // use current job run number as a tag - '0.0.run_number'
-                +"connecta.registry.jetbrains.space/p/connecta/containers/api:latest"
-            }
-        }
-    }
+//    host("Build and push a Docker image...") {
+//        dockerBuildPush {
+//            // by default, the step runs not only 'docker build' but also 'docker push'
+//            // to disable pushing, add the following line:
+//            // push = false
+//
+//            // path to Docker context (by default, context is working dir)
+////             context = "docker buildx -f docker/Dockerfile.test --platform linux/arm64 && echo"
+//            // path to Dockerfile relative to the project root
+//            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+//            file = "docker/Dockerfile.test"
+//            // build-time variables
+//            // image labels
+//            labels["vendor"] = "connecta"
+////            platform = "linux/amd64,linux/arm64"
+//            // to add a raw list of additional build arguments, use
+//            extraArgsForBuildCommand = listOf("--no-cache")
+//            // to add a raw list of additional push arguments, use
+//            // extraArgsForPushCommand = listOf("...")
+//            // image tags
+//            tags {
+//                // use current job run number as a tag - '0.0.run_number'
+//                +"connecta.registry.jetbrains.space/p/connecta/containers/api:latest"
+//            }
+//        }
+//    }
 
     // for optimisation puporse: do not run any container, put script just on "host": https://www.jetbrains.com/help/space/jobs-and-actions.html#main-features-of-jobs-and-steps
     host("SSH to Production") {
@@ -61,7 +61,7 @@ job("Run on git push") {
             content = """
                 echo ${'$'}KEYBASE64 | base64 --decode > id_rsa
                 chmod 400 id_rsa
-                ssh -i id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet ${'$'}SSH_USER@${'$'}SERVER_IP "docker login connecta.registry.jetbrains.space --username ${'$'}HUB_USER --password "${'$'}HUB_TOKEN" && sh deploy_api.sh"
+                ssh -i id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet ${'$'}SSH_USER@${'$'}SERVER_IP "docker login connecta.registry.jetbrains.space --username ${'$'}HUB_USER --password "${'$'}HUB_TOKEN" && cd api && git pull origin test && docker build --file docker/Dockerfile.test --label "vendor=connecta" --tag connecta.registry.jetbrains.space/p/connecta/containers/api:latest --platform "linux/arm64" . && docker push connecta.registry.jetbrains.space/p/connecta/containers/api:latest && sh ../deploy_api.sh"
             """
         }
     }
