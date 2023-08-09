a29935240fee58b38b90b475d69d000ff94d17ca
Matvey <motiio.none@gmail.com>
2023-08-09 17:30:18 +0300

    Fix error msg

--- api/src/modules/auth/domain/exceptions.py
+++ api/src/modules/auth/domain/exceptions.py
@@ -7,12 +7,15 @@ from starlette.status import (
 
 InvalidInitData = HTTPException(
     status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-    detail=[{"msg": "Invalid init datat"}],
+    detail={"msg": "Invalid init datat"},
 )
 
 InvalidJWTToken = HTTPException(
     status_code=HTTP_401_UNAUTHORIZED,
-    detail="Invalid authentication credentials",
+    detail={"msg": "Invalid authentication credentials"},
 )
 
-UserNotFound = HTTPException(status_code=HTTP_404_NOT_FOUND, detail="User not found")
+UserNotFound = HTTPException(
+    status_code=HTTP_404_NOT_FOUND,
+    detail={"msg": "User not found"},
+)
