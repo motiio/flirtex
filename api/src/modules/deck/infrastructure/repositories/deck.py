f8d3ef9acef4b7985e50343111aacf245c78a6de
Matvey <motiio.none@gmail.com>
2023-10-29 16:13:59 +0300

    FIX-1: fix deck bug

--- api/src/modules/deck/infrastructure/repositories/deck.py
+++ api/src/modules/deck/infrastructure/repositories/deck.py
@@ -21,7 +21,7 @@ class DeckRepository(
         return None
 
     async def generate(self, *, profile_id: UUID) -> list[UUID]:
-        q = select(column("profile_id")).select_from(func.deck(profile_id))
+        q = select(column("profile_id")).select_from(func.core.deck(profile_id))
         deck = await self._db_session.execute(q)
 
         return list(deck.scalars().all())
