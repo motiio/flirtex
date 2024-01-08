f18e0ebb29fd3699a0ae8813170f06c4860f4156
Matvey <motiio.none@gmail.com>
2024-01-08 12:08:32 +0300

    Fix deploy

--- .space.kts
+++ .space.kts
@@ -1,19 +1,16 @@
-job("Run on git push") {
+job("Run on git push on ./api/") {
     startOn {
         gitPush {
             branchFilter {
                 +"refs/heads/test"
             }
             pathFilter {
-                +"api/"
+                +"./api/"
             }
         }
     }
-// for optimisation puporse: do not run any container, put script just on "host": https://www.jetbrains.com/help/space/jobs-and-actions.html#main-features-of-jobs-and-steps
-    host("SSH to Test env") {
+    host("Start deployment API to TEST server") {
         env["KEYBASE64"] = "{{ project:KEYBASE64 }}"
-        env["HUB_USER"] = "{{ project:JB_DOCKER_LOGIN }}"
-        env["HUB_TOKEN"] = "{{ project:JB_DOCKER_PASS }}"
         env["SSH_USER"] = "{{ project:TEST_SSH_USER }}"
         env["SSH_PASS"] = "{{ project:TEST_SSH_PASS }}"
         env["SERVER_IP"] = "{{ project:TEST_SERVER_IP }}"
@@ -27,8 +24,111 @@ job("Run on git push") {
                     -o StrictHostKeyChecking=no \
                     -o LogLevel=quiet \
                     ${'$'}SSH_USER@${'$'}SERVER_IP "\
-                     cd api/api/docker && git pull origin test \
-                     && docker-compose up -d"
+                     cd backend/api && \ 
+                     git pull origin test &&\
+                     docker-compose up fastapi_app -d"
+            """
+        }
+    }
+}
+
+job("Run on git push on ./pg-db/") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/test"
+            }
+            pathFilter {
+                +"./api/"
+            }
+        }
+    }
+    host("Start deployment api to PI") {
+        env["KEYBASE64"] = "{{ project:KEYBASE64 }}"
+        env["SSH_USER"] = "{{ project:TEST_SSH_USER }}"
+        env["SSH_PASS"] = "{{ project:TEST_SSH_PASS }}"
+        env["SERVER_IP"] = "{{ project:TEST_SERVER_IP }}"
+
+        shellScript {
+            content = """
+                echo ${'$'}KEYBASE64 | base64 --decode > id_rsa
+                chmod 400 id_rsa
+                ssh -i id_rsa \
+                    -o UserKnownHostsFile=/dev/null \
+                    -o StrictHostKeyChecking=no \
+                    -o LogLevel=quiet \
+                    ${'$'}SSH_USER@${'$'}SERVER_IP "\
+                     cd backend/api && \ 
+                     git pull origin test &&\
+                     docker-compose up pg-db -d"
+            """
+        }
+    }
+}
+
+job("Run on git push on ./redis/") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/test"
+            }
+            pathFilter {
+                +"./redis/"
+            }
+        }
+    }
+    host("Start deployment api to PI") {
+        env["KEYBASE64"] = "{{ project:KEYBASE64 }}"
+        env["SSH_USER"] = "{{ project:TEST_SSH_USER }}"
+        env["SSH_PASS"] = "{{ project:TEST_SSH_PASS }}"
+        env["SERVER_IP"] = "{{ project:TEST_SERVER_IP }}"
+
+        shellScript {
+            content = """
+                echo ${'$'}KEYBASE64 | base64 --decode > id_rsa
+                chmod 400 id_rsa
+                ssh -i id_rsa \
+                    -o UserKnownHostsFile=/dev/null \
+                    -o StrictHostKeyChecking=no \
+                    -o LogLevel=quiet \
+                    ${'$'}SSH_USER@${'$'}SERVER_IP "\
+                     cd backend/api && \ 
+                     git pull origin test &&\
+                     docker-compose up redis -d"
+            """
+        }
+    }
+}
+
+job("Run on git push on ./rabbitmq/") {
+    startOn {
+        gitPush {
+            branchFilter {
+                +"refs/heads/test"
+            }
+            pathFilter {
+                +"./redis/"
+            }
+        }
+    }
+    host("Start deployment api to PI") {
+        env["KEYBASE64"] = "{{ project:KEYBASE64 }}"
+        env["SSH_USER"] = "{{ project:TEST_SSH_USER }}"
+        env["SSH_PASS"] = "{{ project:TEST_SSH_PASS }}"
+        env["SERVER_IP"] = "{{ project:TEST_SERVER_IP }}"
+
+        shellScript {
+            content = """
+                echo ${'$'}KEYBASE64 | base64 --decode > id_rsa
+                chmod 400 id_rsa
+                ssh -i id_rsa \
+                    -o UserKnownHostsFile=/dev/null \
+                    -o StrictHostKeyChecking=no \
+                    -o LogLevel=quiet \
+                    ${'$'}SSH_USER@${'$'}SERVER_IP "\
+                     cd backend/api && \ 
+                     git pull origin test &&\
+                     docker-compose up rabbitmq -d"
             """
         }
     }
