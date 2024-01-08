4f0422f4ce6b8f338e00e0472f25b6c55aef31cb
Matvey <motiio.none@gmail.com>
2024-01-08 12:26:25 +0300

    Fix samall bugs

--- api/src/modules/deck/application/dtos/deck.py
+++ api/src/modules/deck/application/dtos/deck.py
@@ -13,7 +13,7 @@ from src.modules.profile.domain.entities.types import Location
 class DeckProfileOutDTO(BaseDTO):
     id: UUID
     name: str
-    bio: str
+    bio: str | None
     age: int
     gender: profile_enums.GenderEnum
     interests: list[InterestOutDTO] | None = None
