01365c9c46308e0687fcff6e5dd346b93c958b45
Matvey <motiio.none@gmail.com>
2024-02-12 20:32:05 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -1,11 +1,38 @@
 job("[PROD]. API deploy") {
     startOn {}
 
+    container(displayName = "Build with Poetry", image = "python:3.12.2-bookworm") {
+        cache {
+            // Генерация имени файла кэша
+            // Использование хэша файла pyproject.toml гарантирует, что все запуски задач с
+            // одинаковым pyproject.toml будут использовать кэшированные зависимости
+            storeKey = "poetry-{{ hashFiles('pyproject.toml') }}"
+
+            // Вариант восстановления
+            // Если нужный файл кэша не найден, использовать кэш из 'poetry-master.tar.gz'
+            restoreKeys {
+                +"poetry-master"
+            }
+
+            // Локальный путь к директории файла кэша
+            localPath = ".venv"
+        }
+
+        shellScript {
+            content = """
+            poetry config virtualenvs.create false
+            poetry install
+            """
+        }
+    }
+
     parameters {
         text("env", value = "prod") {
             options("dev", "prod")
         }
     }
+
+
 
     host("Start deployment API to TEST server") {
         shellScript {
@@ -18,3 +45,4 @@ job("[PROD]. API deploy") {
         }
     }
 }
+
