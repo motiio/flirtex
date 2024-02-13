8a9352aace4675922d08e5c71acf639500b72179
Matvey <motiio.none@gmail.com>
2024-02-13 06:12:18 +0300

    MAIN: create ci/cd

--- api/src/modules/auth/tests/infrastructure/repositories/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.modules.auth.tests.infrastructure.repositories.refresh_token import (
-    RefreshTokenRepository,
-)
-from src.modules.auth.tests.infrastructure.repositories.user import UserRepository
-
-__all__ = ["UserRepository", "RefreshTokenRepository"]
