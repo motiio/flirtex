b294b5e8238031b9dae4f6ecb45db3b568f54072
Matvey <motiio.none@gmail.com>
2024-02-13 18:47:55 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -44,7 +44,7 @@ job("[PROD]. API deploy") {
         build {
             file = "./api/docker/Dockerfile"
             labels["vendor"] = "flirtex"
-            args["WORKERS_COUNT"] = "project:WORKERS_COUNT"
+            args["WORKERS_COUNT"] = "{{ project:WORKERS_COUNT }}"
         }
         push("flirtex.registry.jetbrains.space/p/connecta/containers/api") {
             tags {
