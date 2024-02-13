ce197c118ef8906f5beb99bc5c4071f65220e0ea
Matvey <motiio.none@gmail.com>
2024-02-13 06:08:01 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -24,7 +24,7 @@ job("[PROD]. API deploy") {
             poetry config virtualenvs.create true
             poetry config virtualenvs.in-project true
             poetry install --no-root
-            poetry run pytest --cov=src --cov-report=term --cov-report=term
+            poetry run pytest --cov=src --cov-report=term --cov-config=.coveragerc
             """
         }
     }
