023ab5594eefeb5e9ddd067bc6d15d9e72ff3ad2
Matvey <motiio.none@gmail.com>
2024-02-15 16:10:48 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -1,10 +1,34 @@
-job("Example shell scripts") {
-    container(image = "ubuntu") {
+job("API Tests") {
+    startOn {}
+
+
+    container(displayName = "Testing...", image = "flirtex.registry.jetbrains.space/p/connecta/containers/3.12.2-poetry:latest") {
+        cache {
+            // Генерация имени файла кэша
+            // Использование хэша файла pyproject.toml гарантирует, что все запуски задач с
+            // одинаковым pyproject.toml будут использовать кэшированные зависимости
+            storeKey = "poetry-{{ hashFiles('api/pyproject.toml') }}"
+
+            // Вариант восстановления
+            // Если нужный файл кэша не найден, использовать кэш из 'poetry-master.tar.gz'
+            restoreKeys {
+                +"poetry-master"
+            }
+
+            // Локальный путь к директории файла кэша
+            localPath = "api/.venv"
+        }
+
         shellScript {
-            interpreter = "/bin/bash"
             content = """
-                echo 'Blya'
+            cd api
+            poetry config virtualenvs.create true
+            poetry config virtualenvs.in-project true
+            poetry install --no-root
+            poetry run pytest --cov=src --cov-report=term --cov-config=.coveragerc
             """
         }
+
     }
 }
+
