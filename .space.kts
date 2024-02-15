2d0779f33fa167684db554415775f4a181788bd6
Matvey <motiio.none@gmail.com>
2024-02-15 23:04:10 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -168,15 +168,15 @@ job("API Build and deploy") {
                         -o StrictHostKeyChecking=no \
                         -o LogLevel=INFO \
                         -p ${'$'}SSH_PORT
-                        ${'$'}SSH_USER@${'$'}SSH_HOST \\
-                        rm -rf /usr/local/src/flirtex/api/ \
-                        mkdir -p /usr/local/src/flirtex/api/ \ 
+                        ${'$'}SSH_USER@${'$'}SSH_HOST "\
+                        rm -rf /usr/local/src/flirtex/api/
+                        mkdir -p /usr/local/src/flirtex/api/
                         echo Start downloading hash ${'$'}VENV_HASH.tar.gz
                         curl -f -L \
                             -H 'Authorization: Bearer ${'$'}CACHE_ACCESS_KEY' \
                             https://files.pkg.jetbrains.space/flirtex/p/connecta/default-automation-caches/caches/backend/${'$'}VENV_HASH.tar.gz \
                             --output '/usr/local/src/flirtex/'
-                        echo Start downloading artifacts ${'$'}ARTIFACTS_PATH \
+                        echo Start downloading artifacts ${'$'}ARTIFACTS_PATH
                         curl -f -L \
                             -H 'Authorization: Bearer ${'$'}ARTIFACTS_ACCESS_KEY' \
                             https://files.pkg.jetbrains.space/flirtex/p/connecta/${'$'}ARTIFACTS_PATH \
