2afe832e13cfa9febfe72c45a6affd50214b818c
Matvey <motiio.none@gmail.com>
2024-02-15 19:39:13 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -44,31 +44,31 @@ job("API Build and deploy") {
     }
 
     container(displayName = "Testing...", image = "flirtex.registry.jetbrains.space/p/connecta/containers/3.12.2-poetry:latest") {
-        cache {
+//        cache {
             // Генерация имени файла кэша
             // Использование хэша файла pyproject.toml гарантирует, что все запуски задач с
             // одинаковым pyproject.toml будут использовать кэшированные зависимости
-            storeKey = "poetry-{{ hashFiles('api/pyproject.toml') }}"
+//            storeKey = "poetry-{{ hashFiles('api/pyproject.toml') }}"
 
             // Вариант восстановления
             // Если нужный файл кэша не найден, использовать кэш из 'poetry-master.tar.gz'
-            restoreKeys {
-                +"poetry-master"
-            }
+//            restoreKeys {
+//                +"poetry-master"
+//            }
 
             // Локальный путь к директории файла кэша
-            localPath = "api/.venv"
-        }
+//            localPath = "api/.venv"
+//        }
 
-        shellScript {
-            content = """
-            cd api
-            poetry config virtualenvs.create true
-            poetry config virtualenvs.in-project true
-            poetry install --no-root
-            poetry run pytest --cov=src --cov-report=term --cov-config=.coveragerc
-            """
-        }
+//        shellScript {
+//            content = """
+//            cd api
+//            poetry config virtualenvs.create true
+//            poetry config virtualenvs.in-project true
+//            poetry install --no-root
+//            poetry run pytest --cov=src --cov-report=term --cov-config=.coveragerc
+//            """
+//        }
         fileArtifacts {
             localPath = "api/"
             // Fail job if build/publish/app/ is not found
