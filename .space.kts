a1897f6dba1c0958f5fad5bd4a44c7692d710d04
Matvey <motiio.none@gmail.com>
2024-02-15 20:37:12 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -159,11 +159,12 @@ job("API Build and deploy") {
             content = """
                     echo ${'$'}DEPLOY_PK | base64 --decode > id_rsa
                     chmod 400 id_rsa
+                    mkdir -p ~/.ssh/known_hosts
                     ssh-keyscan -p ${'$'}SSH_PORT ${'$'}SSH_HOST >> ~/.ssh/known_hosts
                     scp -i id_rsa \
                         -P ${'$'}SSH_PORT \
                         -r ./api/* \
-                        ${'$'}SSH_USER@${'$'}SSH_HOST:/usr/loacl/src/flirtex/api/
+                        ${'$'}SSH_USER@${'$'}SSH_HOST:/usr/local/src/flirtex/api/
               """
         }
         requirements {
