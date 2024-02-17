abd5907a7a89f2b259d21e9422633d36362e6d48
Matvey <motiio.none@gmail.com>
2024-02-17 16:05:44 +0300

    MAIN: create ci/cd

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -4,8 +4,16 @@ set -e
 
 # activate our virtual environment here
 . /opt/pysetup/.venv/bin/activate
-# You can put other setup logic here
-alembic upgrade head
 
-# Evaluating passed command:
-exec "$@"
+# You can put other setup logic here
+
+# Если скрипту передана команда (например, bash для отладки),
+# выполнить переданную команду вместо запуска gunicorn:
+if [ "$1" ]; then
+  exec "$@"
+else
+  # Выполнение миграций
+  alembic upgrade head
+  # Запуск Gunicorn
+  exec gunicorn src.main:api --workers ${WORKERS_COUNT:-4} -k uvicorn.workers.UvicornWorker --bind unix:/tmp/api.sock
+fi
