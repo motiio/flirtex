169ee93e883c3dc5aecb202ea193a3dc22844907
Matvey <motiio.none@gmail.com>
2024-02-17 15:49:36 +0300

    MAIN: create ci/cd

--- .space.kts
+++ .space.kts
@@ -213,8 +213,7 @@ job("API Build and deploy") {
                         JWT_REFRESH_TOKEN_EXPIRE_SECONDS=${'$'}JWT_REFRESH_TOKEN_EXPIRE_SECONDS \
                         REDIS_NOTIFIER_URL=${'$'}REDIS_NOTIFIER_URL \
                         S3_PHOTO_BUCKET_NAME=${'$'}S3_PHOTO_BUCKET_NAME \
-                        WORKERS_COUNT=${'$'}WORKERS_COUNT && \
-                        docker compose build --no-cache api && \
+                        docker compose build --build-arg WORKERS_COUNT={'${'$'}'}WORKERS_COUN --no-cache api && \
                         docker image prune -f --filter "until=240h" && \
                         docker compose up api -d
                         "
--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 
 # activate our virtual environment here
 . /opt/pysetup/.venv/bin/activate
-
 # You can put other setup logic here
+alembic upgrade head
 
 # Evaluating passed command:
 exec "$@"
