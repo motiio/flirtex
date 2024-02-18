86dd3e1ee39fc383faac1c00e5e46a88f3416adb
Matvey <motiio.none@gmail.com>
2024-02-18 17:17:50 +0300

    MAIN: create ci/cd

---
+++ nginx/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Функция для получения сертификатов с Certbot
+get_certificates() {
+    certbot certonly --nginx \
+    -d flirtex.fun -d www.flirtex.fun \
+    --non-interactive \
+    --agree-tos \
+    --email motiio.none@gmail.com \
+    --no-eff-email \
+    --keep-until-expiring
+}
+
+# Функция для автоматического обновления сертификатов
+auto_renew() {
+    echo "0 0,12 * * * root certbot renew --quiet --post-hook 'nginx -s reload'" >> /etc/crontabs/root
+    crond
+}
+
+# Вызов функции для получения или обновления сертификатов
+get_certificates
+
+# Настройка crontab для автоматического обновления сертификатов
+auto_renew
+
+# Запуск Nginx в foreground
+exec nginx -g 'daemon off;'
